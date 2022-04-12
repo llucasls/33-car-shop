f9354d32e1dffb79b7198ad868ecd4bae56da88a
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-12 18:37:05 -0300

    refactor: valida interfaces com zod

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,6 +1,9 @@
-import { Vehicle } from './VehicleInterface';
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
 
-export interface Car extends Vehicle {
-  doorsQty: number,
-  seatsQty: number,
-}
+export const CarSchema = VehicleSchema.extend({
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+export type Car = z.infer<typeof CarSchema>;
