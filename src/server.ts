d4bb869796fbd80cbbf5c7508195dd697a8dd135
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-19 18:25:13 -0300

    feat: cria entidade carro
    - Cria novas instâncias das classes CarController
      e CarRouter.
    - Adiciona rotas de carro à aplicação.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
 import App from './app';
+import { Car } from './interfaces/CarInterface';
+import CarController from './controllers/car';
+import CustomRouter from './routes';
 
 const server = new App();
+
+const carController = new CarController();
+
+const carRouter = new CustomRouter<Car>();
+carRouter.addRoute(carController);
+
+server.addRouter(carRouter.router);
 
 export default server;
