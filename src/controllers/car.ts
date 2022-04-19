9886a1f758d12565a68dc4281d47dcd91ddbbb59
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-19 20:21:15 -0300

    feat: cria classe de controladores de carros

---
+++ src/controllers/car.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from 'express';
+import Controller, { RequestWithBody, ResponseError } from './MongoController';
+import CarService from '../services/car';
+import { Car } from '../interfaces/CarInterface';
+
+class CarController extends Controller<Car> {
+  private _route: string;
+
+  constructor(
+    service = new CarService(),
+    route = '/cars',
+  ) {
+    super(service);
+    this._route = route;
+  }
+
+  get route() { return this._route; }
+
+  create = async (
+    req: RequestWithBody<Car>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { body } = req;
+    try {
+      const car = await this.service.create(body);
+      if (!car) {
+        return res.status(500).json({ error: this.errors.internal });
+      }
+      if ('error' in car) {
+        return res.status(400).json(car);
+      }
+      return res.status(201).json(car);
+    } catch (err) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  readOne = async (
+    req: Request<{ id: string; }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      const car = await this.service.readOne(id);
+      return car
+        ? res.json(car)
+        : res.status(404).json({ error: this.errors.notFound });
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  update = async (req: RequestWithBody<Car>, res: Response) => {
+    try {
+      const { body } = req;
+      const { id } = req.params;
+      const car = await this.service.update(id, body);
+      return res.status(200).json(car);
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  delete = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const car = await this.service.delete(id);
+      return res.status(200).json(car);
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+}
+
+export default CarController;
