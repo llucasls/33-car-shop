81f0c242b4cddda91d2c97cd9e45518abcf983f3
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-20 11:27:42 -0300

    feat: valida método readOne

--- src/services/MongoService.ts
+++ src/services/MongoService.ts
@@ -1,4 +1,5 @@
 import { ZodError } from 'zod';
+import HttpError from '../utils/HttpError';
 // import MongoModel from '../models/MongoModel';
 import { Model } from '../interfaces/ModelInterface';
 
@@ -8,7 +9,14 @@ export interface ServiceError {
 
 export type Service<T> = T | ServiceError | null;
 
+enum ServiceErrors {
+  invalidIdFormat = 'Id must have 24 hexadecimal characters',
+  notFound = 'Object not found',
+}
+
 abstract class MongoService<T> {
+  protected errors = ServiceErrors;
+
   constructor(protected model: Model<T>) { }
 
   public async create(obj: T): Promise<Service<T>> {
@@ -20,7 +28,15 @@ abstract class MongoService<T> {
   }
 
   public async readOne(id: string): Promise<Service<T>> {
-    return this.model.readOne(id);
+    const idFormat = /^[0-9a-f]{24}$/;
+    if (!idFormat.test(id)) {
+      throw new HttpError(400, this.errors.invalidIdFormat);
+    }
+    const obj = await this.model.readOne(id);
+    if (!obj) {
+      throw new HttpError(404, this.errors.notFound);
+    }
+    return obj;
   }
 
   public async update(id: string, obj: T): Promise<Service<T>> {
