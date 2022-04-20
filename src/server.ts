d5e0958b7cedb58a940b71347f2c13e6af44bc07
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-20 11:20:39 -0300

    feat: utiliza os middlewares de erro

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import App from './app';
 import { Car } from './interfaces/CarInterface';
 import CarController from './controllers/car';
 import CustomRouter from './routes';
+import errorHandler from './middlewares/ErrorHandler';
+import notFoundHandler from './middlewares/NotFoundHandler';
 
 const server = new App();
 
@@ -11,5 +13,8 @@ const carRouter = new CustomRouter<Car>();
 carRouter.addRoute(carController);
 
 server.addRouter(carRouter.router);
+
+server.handleError(errorHandler);
+server.handleError(notFoundHandler);
 
 export default server;
