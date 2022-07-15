460bb482d82a5154c8bd4a69d7acafcf8e367bb1
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-15 19:47:17 -0300

    refactor: change connection URI

--- src/connection.ts
+++ src/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/CarShop';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL,
